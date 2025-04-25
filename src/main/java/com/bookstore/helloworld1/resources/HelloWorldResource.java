4f58f9edb2ee1b920569318d7f575bcdcbb7bc63
Venuvk0805 <rvenukanth5@gmail.com>
2025-04-25 14:49:50 +0530

    Modified HelloWorldResource class to getUsers by api

--- src/main/java/com/bookstore/helloworld1/resources/HelloWorldResource.java
+++ src/main/java/com/bookstore/helloworld1/resources/HelloWorldResource.java
@@ -4,19 +4,36 @@
  */
 package com.bookstore.helloworld1.resources;
 
+import com.bookstore.helloworld1.model.User;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.GET;
 import  javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
-@Path("/helloVK")
+@Path("/hello")
 public class HelloWorldResource {
     
+    private static List<User> users =new ArrayList<>();
+    
+    static{
+        users.add(new User(1,"Venukanth","rvenukanth5@gmail.com"));
+        users.add(new User(2,"Kithuskanth","kithus3457@gmail.com"));
+        users.add(new User(3,"Piranav","sppirana@gmail.com"));
+    }
+    
+//    @GET
+//    @Produces(MediaType.TEXT_PLAIN)
+//    public String getHello(){
+//        return "Hello ! This is Venu";
+//    }
+    
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getHello(){
-        return "Hello ! This is Venu";
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> getUsers(){
+        return users;
     }
     
 }
